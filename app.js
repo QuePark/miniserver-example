ac06701e2f39c796c36b72573ad4f3e6c211e06a
Gyuha Park <gyuhapark11@gmail.com>
2023-03-20 14:33:09 +0900

    Update endpoint

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.get("/data/dummy-news", async (req, res) => {
 });
 
 app.get("/data/mini-weather", async (req, res) => {
-  res.status(200).json({ status: "sunny", tempature: "19", finedust: "good" }).end();
+  res.status(200).json({ status: "sunny", temperature: "19", findust: "good" }).end();
 });
 
 const server = app.listen(5000);
 
-console.log("http://localhost:5000/data/latestNews 에서 뉴스를 얻을 수 있습니다");
-console.log("http://localhost:5000/data/weather 에서 날씨를 얻을 수 있습니다");
+console.log("http://localhost:5000/data/dummy-news 에서 뉴스를 얻을 수 있습니다");
+console.log("http://localhost:5000/data/mini-weather 에서 날씨를 얻을 수 있습니다");
 
 module.exports = {
   app,
